6259f3e80056199aec8413732d01cc39b1c690f7
mbunse <mbunse@hotmail.com>
2018-03-09 22:22:54 +0000

    Initial import

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/bin/python
+
+from setuptools import setup, find_packages
+
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(name='socket_client_server',
+      version='0.1.dev1',
+      description='UNIX domain socket client and server classes',
+      keywords='server client unix socket daemon'
+      url='http://github.com/storborg/funniest',
+      author='Moritz Bunse',
+      author_email='mbunse@hotmail.com',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[],
+      python_requires='2.7.*'
+      )
